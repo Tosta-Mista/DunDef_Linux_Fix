a24183a4320a6fe0e631823d099e938cc7f24098
José Gonçalves <jose.goncalves@dlcproduction.ch>
2014-08-04 23:08:44 +0200

    Add a new workaround to the script.

--- DungeonDefenderLinux_Fix.sh
+++ DungeonDefenderLinux_Fix.sh
@@ -2,11 +2,24 @@
 
 ###########################
 # Workaround for Dungeon defender Eternity on linux
-#
-# Author : José Gonçalves
 ###########################
 
-DUNDEFPATH=".steam/steam/SteamApps/common/DunDefEternity/DunDefEternityLauncher"
+# SET PATH :
+## Dungeaon defender launcher path 
+DUNDEFPATH="$HOME/.steam/steam/SteamApps/common/DunDefEternity/DunDefEternityLauncher"
+## Steam libs path
+STEAMPATH="$HOME/.steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/*"
+## Dnndef Binaries path
+DUNDEFPATHLINK="$HOME/.steam/steam/SteamApps/common/DunDefEternity/DunDefEternity/Binaries/Linux/"
+
+function cleaning {
+	unset DUNDEFPATH 
+	unset DUNDEFPATHLINK
+	unset STEAMPATH
+	unset unlib
+	unset avlib
+	unset choice
+}
 
 ## Check for unavailble libs :
 echo "Checking for unavailable libs..."
@@ -20,25 +33,53 @@ avlib=`ldd $DUNDEFPATH | grep lib  | tr "\t" " " | cut -d"=" -f1`
 echo $avlib
 echo ""
 
-# Installing Main libs
-## activation of i386 arch
-echo "Add i386 arch"
-sudo dpkg-add-architecture i386
+while true; do
+	echo "What workaround do you want?"
+	echo " 1 - Symlink way (Work on Redhat/Fedora and Debian/Ubuntu) "
+	echo " 2 - Installing package way (Works on Debian/Ubuntu 64 bit) "
+	echo " Q - Quit"
+	echo -e "Your choice : "
+	read choice
 
-echo "Installing missing libs :"
-sudo aptitude install -y libsfml-dev:i386 libcrypto++-dev:i386 libcurl4-nss-dev:i386 libcurl4-openssl-dev:i386 libfreetype6:i386 libxrandr2:i386 libgtk2.0-0:i386 libpango-1.0-0:i386 libpangocairo-1.0-0:i386 libasound2-dev:i386 libgdk-pixbuf2.0-0:i386
-echo ""
+	case $choice in 
+		1)
+			ln -sf $STEAMPATH $DUNDEFPATHLINK
+			clear;
+			echo "Symlink Done!"
+			echo "#################################################"
+			echo "# Try to launch Dungeon Defender Steam	      #"
+			echo "#################################################"
+			cleaning
+			exit 0;
+			;;
+		2)
+			# Installing Main libs
+			## activation of i386 arch
+			echo "Add i386 arch"
+			sudo dpkg-add-architecture i386
+			
+			echo "Installing missing libs :"
+			sudo aptitude install -y libsfml-dev:i386 libcrypto++-dev:i386 libcurl4-nss-dev:i386 libcurl4-openssl-dev:i386 libfreetype6:i386 libxrandr2:i386 libgtk2.0-0:i386 libpango-1.0-0:i386 libpangocairo-1.0-0:i386 libasound2-dev:i386 libgdk-pixbuf2.0-0:i386
+			echo ""
+			
+			echo "Checking unavailable libs (If you have nothing is good :))"
+			echo `ldd $DUNDEFPATH | grep "not found" | tr "\t" " " | cut -d"=" -f1`
+			echo ""
+			
+			echo "# ATTENTION : ##############################################################" 
+			echo "# If OpenGL is no more supported, please reinstall your NVIDIA/ATI Drivers #"
+			echo "############################################################################"
+			cleaning
+			exit 0;
+			;;
+		Q|q)
+			cleaning
+			exit 0;	
+			;;
+		*)
+			echo "Please choose somthing available on the list..."
+			;;
+	esac
+done
 
-echo "Checking unavailable libs (If you have nothing is good :))"
-echo `ldd $DUNDEFPATH | grep "not found" | tr "\t" " " | cut -d"=" -f1`
-echo ""
-
-
-echo "# ATTENTION : ##############################################################" 
-echo "# If OpenGL is no more supported, please reinstall your NVIDIA/ATI Drivers #"
-echo "############################################################################"
-
-# Cleaning 
-unset DUNDEFPATH
-unset unlib
-unset avlib
+exit 0;
