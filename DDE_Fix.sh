347cd39fd1c1da4250318a0ecd6b9ab689c37f66
José Gonçalves <jose.goncalves@dlcproduction.ch>
2014-08-16 03:01:36 +0200

    - Clean script - Factorised my functions - Auto-detection of steam path and other paths - Remove Adrian Fix - Add content to Readme File. - Rename some functions - Reduce the length of the main script name. - Testing. - Fix.

---
+++ DDE_Fix.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+###########################
+# Workaround for Dungeon defender Eternity on linux
+# --------------------------------------------------
+# Big thanks for the community arround Valve!
+# Thanks for your feedback, reports...
+###########################
+
+# SET PATH :
+STEAMPATH=`locate "steam.pid" | head -1 | awk '{ sub("/[a-z]*.[a-z]*.$", ""); print }'`
+STEAM_LIB_PATH=`locate "steam.pid" | head -1 | awk '{ sub("/[a-z]*.[a-z]*.$", "/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/*"); print }'`
+DUNDEF_LIB_PATH=`locate "steam.pid" | head -1 | awk '{sub("/[a-z]*.[a-z]*.$", "/steam/SteamApps/common/DunDefEternity/DunDefEternity/Binaries/Linux/"); print }'`
+DUNDEF_LAUNCHER_PATH=`locate "steam.pid" | head -1 | awk '{ sub("/[a-z]*.[a-z]*.$", "/steam/SteamApps/common/DunDefEternity/DunDefEternityLauncher"); print }'`
+
+# Function used to ask if the user want to launch the game :
+function GameLaunch () {
+    local step=true
+    while ${step}; do
+        echo -e "Do you want launch Dungeon Defender?(Y/N)"
+        read answer
+        case ${answer} in
+            Y|y)
+                steam steam://rungameid/302270 &
+                step=false
+                ;;
+            N|n)
+                echo "Quiting..."
+                step=false
+                ;;
+            *)
+                echo "Please use Y or N."
+                step=true
+                ;;
+        esac
+    done
+}
+
+function CheckLibs () {
+    ## Check for available libs :
+    echo "Checking for available libs..."
+    echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep lib  | tr "\t" " " | cut -d"=" -f1`
+    echo ""
+
+    ## Check for unavailable libs :
+    echo "Checking for unavailable libs..."
+    echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep "not found" | tr "\t" " " | cut -d"=" -f1`
+    echo ""
+
+    echo "Directories used for your libs"
+    # Prints out all directory used to provide your libs
+    sudo ldconfig -v 2>/dev/null | grep -v ^$'\t'
+}
+
+# Not an Edge Fix this workaround seems work well on SteamOS linux flavour.
+function SymLinkFix () {
+    ## Check for available libs :
+    echo "Checking for available libs..."
+    echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep lib  | tr "\t" " " | cut -d"=" -f1`
+    echo ""
+
+    echo "Checking for unavailable libs..."
+    echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep "not found" | tr "\t" " " | cut -d"=" -f1`
+    echo ""
+
+    ## Doing job
+    ln -sf ${STEAM_LIB_PATH} ${DUNDEF_LIB_PATH}
+    clear;
+    echo "Symlinking Done!"
+}
+
+# PandaFix workaround (For Debian/Ubuntu/Kubuntu etc... ) install the package needed and afterwards the libs are provided
+# by LD path :). To check your LD path ldconfig -v
+function PandaFix {
+    CheckLibs
+
+    # Installing Main libs
+    ## Debian
+    if [[ -x "$(which aptitude)" ]]; then
+        echo "Add i386 arch"
+        sudo dpkg --add-architecture i386
+
+        echo "Installing missing libs :"
+        sudo aptitude install libgconf-2-4:i386 libvorbisfile3:i386 libsfml-dev:i386 \
+            libcrypto++-dev:i386 libcurl4-nss-dev:i386 libcurl4-openssl-dev:i386 libfreetype6:i386 \
+            libxrandr2:i386 libgtk2.0-0:i386 libpango-1.0-0:i386 libpangocairo-1.0-0:i386 libasound2-dev:i386 \
+            libgdk-pixbuf2.0-0:i386
+    fi
+
+    ## Red Hat
+    if [[ -x "$(which yum)" ]]; then
+        echo "Installing missing libs :"
+        sudo yum install GConf2.i686 GConf2-devel.i686 libvorbis.i686 SFML.i686 SFML-devel.i686 \
+            cryptopp.i686 libcurl.i686 libcurl-devel.i686 freetype.i686 freetype-devel.i686 libXrandr.i686 \
+            libXrandr-devel.i686 gtk2.i686 gtk2-devel.i686 pango.i686 pango-devel.i686 cairo.i686 \
+            cairo-devel.i686 gfk-pixbuf2-devel.i686 gtk-pixbuf2.i686
+    fi
+
+    ## ArchLinux
+    if [[ -x "$(which pacman)" ]]; then
+        echo "Installing missing libs :"
+        echo "Pacman is currently not supported"
+        #sudo pacman -S
+    fi
+
+    echo ""
+    echo "Checking unavailable libs (If you have nothing then you're good :))"
+    echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep "not found" | tr "\t" " " | cut -d"=" -f1`
+    echo ""
+
+    echo "# ATTENTION : ################################################################"
+    echo "# If OpenGL is no longer supported, please reinstall your NVIDIA/ATI Drivers #"
+    echo "##############################################################################"
+}
+
+Cleaning () {
+    echo "Cleaning symlink fix..."
+    find "$DUNDEF_LIB_PATH" -maxdepth 1 -type l -exec rm -f {} \;
+    echo "Cleaning done..."
+}
+
+#Show menu :
+while true; do
+    echo "----------------------------"
+    echo "-[ Choose your workaround ]-"
+    echo "----------------------------"
+    echo " [1] -> SymLink Fix   --> Create all symlinks needed to fix your issue. (All Linux OS)"
+    echo " [2] -> PandaWan Fix  --> Install all package needed for your game. (Apt/Yum manager only.)"
+    echo " [3] -> ShowMyLibs    --> Show all directories used to provide your Libs (All linux OS)"
+    echo " [4] -> Cleaning      --> Remove Symlink Fix!"
+    echo " [Q] -> Quit"
+    echo "---------------------------"
+    echo "Your choice : "
+    read choice
+
+    case ${choice} in
+        1)
+            SymLinkFix
+            GameLaunch
+            exit 0;
+            ;;
+        2)
+            PandaFix
+            GameLaunch
+            exit 0;
+            ;;
+        3)
+            # Prints out all directories used to provide your libs
+            CheckLibs
+            ;;
+        4)
+            Cleaning
+            exit 0;
+            ;;
+        Q|q)
+            exit 0;
+            ;;
+        *)
+            echo "Please choose something available on the list..."
+            ;;
+    esac
+done
--- README.md
+++ README.md
@@ -1,58 +1,11 @@
 DunDef3.2_Linux_Fix
 ===================
-Quick fix for Dungeon Defender Eternity Linux 3.2 build. I can only test this patch on Debian Flavours... 
+Quick fix for Dungeon Defender Eternity on Linux. This scripts should work on all Linux flavours. If you use pacman as
+package manager the pandafix doesn't work.
+
 Archlinux, Gentoo and other linux flavours testers are welcome! => Open an issue with your result.
 
 Thanks in advance.
-## Add a new fix 
-Create your function :
-```shell
-function NameOfFix {
-    # Put your fix here!
-}
-```
-
-Add an entry into the loop :
-```shell
-while true; do
-    echo "What workaround do you want?"
-    echo "----------------------------"
-    echo " 1 - Edge Fix --> Symlink way (Work on Redhat/Fedora and Debian/Ubuntu)"
-    echo " 2 - PandaWan Fix --> Package way (Work on Debian/Ubuntu 64 bit) "
-    echo " 3 - Adrian Fix --> Symlink way (Work on ArchLinux)"
----> new entry here (echo " Number - NameOfYourFix --> Description")
-    echo " Q - Quit"
-    echo "------------------------------------------"
-    echo -e "Your choice : "
-    read choice
-    case ${choice} in
-        1)
-            EdgeFix
-            cleaning
-            exit 0;
-            ;;
-        2)
-            PandaFix
-            cleaning
-            exit 0;
-            ;;
-        3)
-            AdrianFix
-            cleaning
-            exit 0;
-            ;;
----> new entry here ( Number) FunctionName - Cleaningfunction - exit 0; ;;)
-        Q|q)
-            cleaning
-            exit 0;
-            ;;
-        *)
-            echo "Please choose something available on the list..."
-            ;;
-    esac
-done
-```
-
 ## How to use
 ### Download the script :
 #### With Git :<br />
@@ -60,40 +13,24 @@ done
 git clone https://github.com/Tosta-Mixta/DunDef3.2_Linux_Fix.git
 ```
 #### Without Git : <br />
-https://github.com/Tosta-Mixta/DunDef3.2_Linux_Fix/archive/master.zip<br />
+[Download](https://github.com/Tosta-Mixta/DunDef3.2_Linux_Fix/archive/master.zip)
 #### Wget :<br />
 ```
 wget https://github.com/Tosta-Mixta/DunDef3.2_Linux_Fix/archive/master.zip
 ```
 
-### Set your path
-Set PATH constant with your Dungeon defender path.
-```shell
-## Is not an Edge Fix ;) PATH : (Default path for SteamOS)
-EDGE_DUNDEFPATH="/usr/local/games/Steam/SteamApps/common/DunDefEternity/DunDefEternityLauncher"
-EDGE_STEAMPATH="/usr/local/games/Steam/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/*"
-EDGE_DUNDEFPATHLINK="/usr/local/games/Steam/SteamApps/common/DunDefEternity/DunDefEternity/Binaries/Linux/"
-## Panda's fix PATH : (Default path for Ubuntu)
-PANDA_DUNDEFPATH="$HOME/.steam/steam/SteamApps/common/DunDefEternity/DunDefEternityLauncher"
-## Adrian Fix specials paths : (Default path for ArchLinux (not sure, need to install an Archlinux when i can... to do some test))
-ADRIAN_DUNDEFLAUNCHER="/opt/games/steam/SteamApps/common/DunDefEternity/DunDefEternityLauncher"
-ADRIAN_GLIBPATH="$HOME/.local/share/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libdbus-glib-1.so.2"
-ADRIAN_GCONFPATH="$HOME/.local/share/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu/libgconf-2.so.4"
-ADRIAN_DUNDEFPATHARCH="/opt/games/steam/SteamApps/common/DunDefEternity/DunDefEternity/Binaries/Linux"
-```
-
 ### Run
-Add permission to the script :
-```shell
-chmod +x DungeonDefenderLinux_Fix.sh
+Change your script to executable :
+```
+chmod +x DDE_Fix.sh
 ```
 
-Launch :
-```shell
-./DungeonDefenderLinux_Fix.sh
+Run :
+```
+./DDE_Fix.sh
 ```
 
 ## Misc :
 I hope this scripts can help somebody.
 
-Thank you very much @bubylou for your contribution.
+Thank you bubylou for your contribution.
