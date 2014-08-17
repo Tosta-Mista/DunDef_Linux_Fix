4448bf8e5d6890b9b5b6f0d92b9d867821a17fe7
José Gonçalves <jose.goncalves@dlcproduction.ch>
2014-08-17 04:27:04 +0200

    " Add coloration for the script * Change yum lib * Remove a lot of comment.

--- DDE_Fix.sh
+++ DDE_Fix.sh
@@ -6,6 +6,10 @@
 # Thanks for your feedback, reports...
 ###########################
 
+purple='\e[0;35m'
+yellow='\e[1;33m'
+nc='\e[0m'
+
 # Update your locate
 echo "Settings up PATHS..."
 sudo updatedb
@@ -18,24 +22,22 @@ DUNDEF_LIB_PATH="$STEAMAPPS/common/DunDefEternity/DunDefEternity/Binaries/Linux/
 DUNDEF_LAUNCHER_PATH="$STEAMAPPS/common/DunDefEternity/DunDefEternityLauncher"
 
 # List of package used for Debian :
-apt="libgconf-2-4:i386 libvorbisfile3:i386 libsfml-dev:i386 libcrypto++-dev:i386 libcurl4-nss-dev:i386 \
-    libcurl4-openssl-dev:i386 libfreetype6:i386 libxrandr2:i386 libgtk2.0-0:i386 libpango-1.0-0:i386 \
-    libpangocairo-1.0-0:i386 libasound2-dev:i386 libgdk-pixbuf2.0-0:i386"
-# List of package used for RedHat (The game can run fine without all librairies, we can reduce this list):
-yum="GConf2.i686 GConf2-devel.i686 libvorbis.i686 SFML.i686 SFML-devel.i686 cryptopp.i686 libcurl.i686 \
-    libcurl-devel.i686 freetype.i686 freetype-devel.i686 libXrandr.i686 libXrandr-devel.i686 gtk2.i686 \
-    gtk2-devel.i686 pango.i686 pango-devel.i686 cairo.i686 cairo-devel.i686 gfk-pixbuf2-devel.i686 \
-    gtk-pixbuf2.i686"
+apt="libgconf-2-4:i386 libvorbisfile3:i386 libsfml-dev:i386 libcrypto++-dev:i386 curl:i386 libcurl4-openssl-dev:i386 \
+    libfreetype6:i386 libxrandr2:i386 libgtk2.0-0:i386 libpango-1.0-0:i386 libpangocairo-1.0-0:i386 \
+    libasound2-dev:i386 libgdk-pixbuf2.0-0:i386"
+# List of package used for RedHat :
+yum="GConf2.i686 libvorbis.i686 SFML.i686 SFML-devel.i686 cryptopp.i686 libcurl.i686 libcurl-devel.i686 \
+    freetype.i686 freetype-devel.i686 libXrandr.i686 libXrandr-devel.i686 gtk2.i686 gtk2-devel.i686 \
+    pango.i686 pango-devel.i686 cairo.i686 cairo-devel.i686 gtk-pixbuf2-devel.i686 gtk-pixbuf2.i686"
 # List of package used for Arch :
 pacman="gconf lib32-libvorbis sfml crypto++ lib32-libgcrypt curl lib32-nss lib32-openssl lib32-libfreetype \
     lib32-libxrandr lib32-gtk2 lib32-pango libtiger lib32-gdk-pixbuf2"
-
 
 # Function used to ask if the user want to launch the game :
 function LaunchGame () {
     local step=true
     while ${step}; do
-        echo -e "Do you want launch Dungeon Defender?(Y/N)"
+        echo -e "${yellow}Do you want launch Dungeon Defender?(Y/N)${nc}"
         read answer
         case ${answer} in
             Y|y)
@@ -43,11 +45,11 @@ function LaunchGame () {
                 step=false
                 ;;
             N|n)
-                echo "Quiting..."
+                echo -e "${yellow}Quiting...${nc}"
                 step=false
                 ;;
             *)
-                echo "Please use Y or N."
+                echo -e "${yellow}Please use Y or N.${nc}"
                 step=true
                 ;;
         esac
@@ -56,21 +58,23 @@ function LaunchGame () {
 
 function CheckLibs () {
     ## Check for available libs :
-    echo "Checking for available libs..."
+    echo -e "${purple}------------------------------------------------------${nc}"
+    echo -e "${yellow}Installed Libs :${nc}"
     echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep lib  | tr "\t" " " | cut -d"=" -f1`
-    echo ""
+    echo -e "${purple}------------------------------------------------------${nc}"
 
     ## Check for unavailable libs :
-    echo "Checking for unavailable libs..."
+    echo -e "${yellow}Missing Libs :${nc}"
     echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep "not found" | tr "\t" " " | cut -d"=" -f1`
-    echo ""
+    echo -e "${purple}------------------------------------------------------${nc}"
 
-    echo "Directories used for your libs"
+    echo ""
+    echo -e "${yellow}Directories used for your libs${nc}"
     # Prints out all directory used to provide your libs
     sudo ldconfig -v 2>/dev/null | grep -v ^$'\t'
+    echo -e "${purple}------------------------------------------------------${nc}"
 }
 
-# Not an Edge Fix this workaround seems work well on SteamOS linux flavour.
 function SymLinkFix () {
     CheckLibs
 
@@ -79,72 +83,70 @@ function SymLinkFix () {
     ln -sf ${STEAM_LIB_PATH}/lib/i386-linux-gnu/* ${DUNDEF_LIB_PATH}
 
     clear
-    echo "Symlinking Done!"
+    echo -e "${yellow}Symlinking Done!${nc}"
+    echo -e "${purple}------------------------------------------------------${nc}"
+    echo -e "${yellow}Missing libs :${nc}"
+    echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep "not found" | tr "\t" " " | cut -d"=" -f1`
+    echo -e "${purple}------------------------------------------------------${nc}"
 }
 
-# PandaFix workaround (For Debian/Ubuntu/Kubuntu etc... ) install the package needed and afterwards the libs are provided
-# by LD path :). To check your LD path ldconfig -v
 function PandaFix {
     CheckLibs
 
     # Installing Main libs
     ## Debian Flavours
     if [[ -x "$(which aptitude)" ]]; then
-        echo "Add i386 arch"
+        echo -e "${yellow}Add i386 arch${nc}"
         sudo dpkg --add-architecture i386
-
-        echo "Installing missing libs :"
+        echo -e "${yellow}Installing missing libs :${nc}"
         sudo aptitude update && sudo aptitude install ${apt}
 
     elif [[ -x "$(which apt-get)" ]]; then
-        echo "Add i386 arch"
+        echo -e "${yellow}Add i386 arch"
         sudo dpkg --add-architecture i386
-
-        echo "Installing missing libs :"
+        echo -e "${yellow}Installing missing libs :${nc}"
         sudo apt-get update && sudo apt-get install ${apt}
     fi
 
     ## Red Hat Flavours
     if [[ -x "$(which yum)" ]]; then
-        echo "Installing missing libs :"
+        echo -e "${yellow}Installing missing libs :${nc}"
         sudo yum update && sudo yum install ${yum}
     fi
 
     ## ArchLinux Flavours
     if [[ -x "$(which pacman)" ]]; then
-        echo "Enabling 'MultiLib' Repo :"
-        sudo sed 's/#\[multilib\]/\[multilib\]/g' -i /etc/pacman.conf
-        sudo sed 's/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/g' -i /etc/pacman.conf
-
-        echo "Installing missing libs :"
-        echo "Support of pacman package manager is currently in testing..."
+        echo -e "${yellow}Enabling 'MultiLib' Repo :${nc}"
+        sudo sed 's/#\[multilib\]/\[multilib\]/g;s/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/g' -i /etc/pacman.conf
+        echo -e "${yellow}Installing missing libs :${nc}"
+        echo -e "${yellow}/!\ Support of pacman package manager is currently in testing...${nc}"
         sudo pacman -Syy && sudo pacman -S ${pacman}
     fi
 
-    echo ""
-    echo "Checking unavailable libs :"
+    echo -e "${purple}------------------------------------------------------${nc}"
+    echo -e "${yellow}Missing Libs :${nc}"
     echo `ldd ${DUNDEF_LAUNCHER_PATH} | grep "not found" | tr "\t" " " | cut -d"=" -f1`
-    echo ""
+    echo -e "${purple}------------------------------------------------------${nc}"
 }
 
 Cleaning () {
-    echo "Cleaning symlink fix..."
+    echo -e "${yellow}Cleaning symlink fix...${nc}"
     find "$DUNDEF_LIB_PATH" -maxdepth 1 -type l -exec rm -f {} \;
-    echo "Cleaning done..."
+    echo -e "${yellow}Cleaning done...${nc}"
 }
 
 #Show menu :
 while true; do
-    echo "----------------------------"
-    echo "-[ Choose your workaround ]-"
-    echo "----------------------------"
-    echo " [1] -> SymLink Fix   --> Create all symlinks needed to fix your issue. (All Linux OS)"
-    echo " [2] -> PandaWan Fix  --> Install all package needed for your game. (Apt/Yum manager only.)"
-    echo " [3] -> ShowMyLibs    --> Show all directories used to provide your Libs (All Linux OS)"
-    echo " [4] -> Cleaning      --> Remove Symlink Fix!"
-    echo " [Q] -> Quit          --> Exit..."
-    echo "---------------------------"
-    echo "Your choice : "
+    echo -e "${purple}+----------------------------+${nc}"
+    echo -e "${purple}|${nc} ${yellow}[ Choose your workaround ]${nc} ${purple}|${nc}"
+    echo -e "${purple}+----------------------------+------------------------${nc}"
+    echo -e "${purple}|${nc} ${yellow}[1] -> SymLink Fix   --> Create all symlinks needed to fix your issue. (All Linux OS)${nc}"
+    echo -e "${purple}|${nc} ${yellow}[2] -> PandaWan Fix  --> Install all package needed for your game. (All Linux OS)${nc}"
+    echo -e "${purple}|${nc} ${yellow}[3] -> ShowMyLibs    --> Show all directories used to provide your Libs (All Linux OS)${nc}"
+    echo -e "${purple}|${nc} ${yellow}[4] -> Cleaning      --> Remove Symlink Fix!${nc}"
+    echo -e "${purple}|${nc} ${yellow}[Q] -> Quit          --> Exit...${nc}"
+    echo -e "${purple}+-----------------------------------------------------${nc}"
+    echo -e "${yellow}Your choice : ${nc}"
     read choice
 
     case ${choice} in
@@ -167,7 +169,7 @@ while true; do
             exit 0;
             ;;
         *)
-            echo "Please choose something available on the list..."
+            echo -e "${yellow}${choice} is not available"
             ;;
     esac
 done
